8f5a65c691a58ada6cebf34444468319d7774f79
cmhaas19 <chrismhaas@gmail.com>
2015-01-31 17:23:19 -0800

    Updating images and more mobile css changes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,7 +5,25 @@
 	//
 	$(function(){
 
+	    //
+	    // Initialize Mobile Menu
+	    //
 	    $("#slide-nav").mmenu();
+	    
+        //
+	    // Enable swiping for Carousel
+	    //
+	    $(".carousel-inner").swipe({
+
+	        swipeLeft: function () {
+	            $(this).parent().carousel('next');
+	        },
+	        swipeRight: function () {
+	            $(this).parent().carousel('prev');
+	        },
+	        //Default is 75px, set to 0 for demo so any distance triggers swipe
+	        threshold: 0
+	    });
 
 	});
 
